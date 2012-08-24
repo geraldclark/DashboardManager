31c19866909a41d3200d1a5387e9e0f06aa9f1d8
Gerald Clark <gerald.clark@live.com>
2012-08-24 01:23:08 -0400

    Formatting

--- ModuleBuilder/packages/DashboardManager/modules/DashboardManager/logic_hooks/DashboardManager_User_Hooks.php
+++ ModuleBuilder/packages/DashboardManager/modules/DashboardManager/logic_hooks/DashboardManager_User_Hooks.php
@@ -1,27 +1,27 @@
 <?php
 
-    if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-
-    class DashboardManager_User_Hooks
-    {
-        function forceRelationshipPolicy(&$bean, $event, $arguments)
-        {
-            if (
-                isset($arguments['module']) && $arguments['module'] == 'Users'
-                && isset($arguments['related_module']) && $arguments['related_module'] == 'dash_DashboardManager'
-                && isset($arguments['link']) && !empty($arguments['link'])
-                && isset($arguments['relationship']) && !empty($arguments['relationship'])
-                && isset($arguments['related_id']) && !empty($arguments['related_id'])
-                && isset($arguments['id']) && !empty($arguments['id'])
-               )
-            {
-                require_once("modules/dash_DashboardManager/dash_DashboardManager.php");
-
-                $dashManagerObj = new dash_DashboardManager();
-                $dashManagerObj->retrieve($arguments['related_id']);
-                $dashManagerObj->forceRelationshipPolicy($arguments['relationship'], $bean);
-            }
-        }
-    }
+	if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+	
+	class DashboardManager_User_Hooks
+	{
+		function forceRelationshipPolicy(&$bean, $event, $arguments)
+		{
+		    if (
+		        isset($arguments['module']) && $arguments['module'] == 'Users'
+		        && isset($arguments['related_module']) && $arguments['related_module'] == 'dash_DashboardManager'
+		        && isset($arguments['link']) && !empty($arguments['link'])
+		        && isset($arguments['relationship']) && !empty($arguments['relationship'])
+		        && isset($arguments['related_id']) && !empty($arguments['related_id'])
+		        && isset($arguments['id']) && !empty($arguments['id'])
+		       )
+		    {
+		        require_once("modules/dash_DashboardManager/dash_DashboardManager.php");
+		
+		        $dashManagerObj = new dash_DashboardManager();
+		        $dashManagerObj->retrieve($arguments['related_id']);
+		        $dashManagerObj->forceRelationshipPolicy($arguments['relationship'], $bean);
+		    }
+		}
+	}
 
 ?>
