c4463cd23d1b7f248c5e6613d755815ab1584932
Gerald Clark <gerald.clark@live.com>
2012-08-24 01:16:00 -0400

    Comments

--- InstallerPackage/Files/custom/Extension/modules/Administration/Ext/Language/DashboardManager.php
+++ InstallerPackage/Files/custom/Extension/modules/Administration/Ext/Language/DashboardManager.php
@@ -1,6 +1,6 @@
 <?php
 
-    //Dashboard Manager Panel
+    //Dashboard Manager
     $mod_strings['LBL_DASHBOARD_MANAGEMENT'] = 'Dashboard Management';
     $mod_strings['LBL_DASHBOARD_MANAGER'] = 'Dashboard Manager';
     $mod_strings['LBL_DASHBOARD_BACKUPS'] = 'Dashboard Backups';
