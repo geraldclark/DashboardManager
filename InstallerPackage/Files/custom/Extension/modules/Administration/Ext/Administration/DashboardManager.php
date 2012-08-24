325e12c9550344ff7fe13e27726100b1a6e80ac8
Gerald Clark <gerald.clark@live.com>
2012-08-24 01:14:01 -0400

    Comments / Formatting

--- InstallerPackage/Files/custom/Extension/modules/Administration/Ext/Administration/DashboardManager.php
+++ InstallerPackage/Files/custom/Extension/modules/Administration/Ext/Administration/DashboardManager.php
@@ -1,6 +1,7 @@
 <?php
 
-	$admin_option_defs=array();
+    //Dashboard Manager Panel
+    $admin_option_defs=array();
     $admin_option_defs['Administration']['DashboardManager_DashboardManager'] = array('ModuleLoader','LBL_DASHBOARD_MANAGER','LBL_DASHBOARD_MANAGER_DESCRIPTION','index.php?module=dash_DashboardManager');
     $admin_option_defs['Administration']['DashboardManager_DashboardBackup'] = array('Backups','LBL_DASHBOARD_BACKUPS','LBL_DASHBOARD_BACKUPS_DESCRIPTION','index.php?module=dash_DashboardBackups');
-	$admin_group_header[]= array('LBL_DASHBOARD_MANAGEMENT', '', false, $admin_option_defs, '');
+    $admin_group_header[]= array('LBL_DASHBOARD_MANAGEMENT', '', false, $admin_option_defs, '');
--- InstallerPackage/Files/custom/Extension/modules/Home/Ext/Language/DashboardManager.php
+++ InstallerPackage/Files/custom/Extension/modules/Home/Ext/Language/DashboardManager.php
@@ -1,5 +1,6 @@
 <?php
 
+    //Dashboard Manager Panel
     $mod_strings['LBL_ADMIN_VERSION_FAIL'] = 'Dashboard Manager is currently not supported for this version. Please install the latest updated package.';
     $mod_strings['LBL_CONFIRM_RESTORE'] = 'Are you sure you want to restore your default dashboard?';
     $mod_strings['LBL_RESTORE_LINK_TEXT'] = 'Restore My Dashboard';
