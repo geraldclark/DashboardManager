69623d0a4ae76bf2a7953db64783efd5944c313d
cmourizard <www-data@debian5.localdomain>
2012-11-30 13:47:13 +0100

    Issue #1 Add french Translation

---
+++ InstallerPackage/Files/custom/Extension/modules/Administration/Ext/Language/DashboardManager.fr_FR.php
@@ -0,0 +1,8 @@
+<?php
+
+    //Dashboard Manager
+    $mod_strings['LBL_DASHBOARD_MANAGEMENT'] = 'Gestionnaire de page d&#39;accueil';
+    $mod_strings['LBL_DASHBOARD_MANAGER'] = 'Modèles de page d&#39;accueil';
+    $mod_strings['LBL_DASHBOARD_BACKUPS'] = 'Sauvegardes de page d&#39;accueil';
+    $mod_strings['LBL_DASHBOARD_MANAGER_DESCRIPTION'] = 'Gestion des modèles de page d&#39;accueil';
+    $mod_strings['LBL_DASHBOARD_BACKUPS_DESCRIPTION'] = 'Gestion des sauvegardes de page d&#39;accueil';
---
+++ InstallerPackage/SugarModules/relationships/language/dash_DashboardManager.fr_FR.php
@@ -0,0 +1,6 @@
+<?php
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_DASH_DASHBOARDBACKUPS_DASH_DASHBOARDMANAGER_FROM_DASH_DASHBOARDBACKUPS_TITLE'] = 'Sauvegardes utilisateur associées';
+$mod_strings['LBL_DASH_DASHBOARDMANAGER_USERS_FROM_USERS_TITLE'] = 'Utilisateurs : Page d&#39;accueils vérouillées';
+$mod_strings['LBL_DASH_DASHBOARDMANAGER_USERS_1_FROM_USERS_TITLE'] = 'Utilisateurs : Page d&#39;accueil initialisées';
+$mod_strings['LBL_DASH_DASHBOARDMANAGER_USERS_2_FROM_USERS_TITLE'] = 'Utilisateurs : Onglets forcés';
